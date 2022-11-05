b5bfbefc5e0d21de84714ee3c07a0b9426c7bc23
mohammedyaseen2211 <ysn6040@gmail.com>
2022-11-05 23:38:46 +0530

    added a file

---
+++ array/js/concat.js
@@ -0,0 +1,16 @@
+// the concat() methods is used to merge two or more arrays
+
+
+const days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"]
+
+console.log(days.concat("week: 7"))
+
+
+//  another example two arryas
+
+
+const a  = [1,2,3,4,5]
+
+const b = (6,7,8,9,10)
+
+console.log(`THe first ten natural numbers : ${a} +  ${b}`) 
