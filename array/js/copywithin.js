9d078804b8abb30f12fe37cbddd78b922296b765
mohammedyaseen2211 <ysn6040@gmail.com>
2022-11-07 00:31:08 +0530

    added a file

---
+++ array/js/copywithin.js
@@ -0,0 +1,25 @@
+// the copywithin() method doesn't copy deep of an array to another location in the same array and returns it without modifying the length.
+
+
+//  In other words :- copywithin method is used to copy the arrays value from the  index value where address is located/intalized i.e index[0] or index[1] .and it will return the  arrays in the sequence manner to their lengths from first. to clear it watch the example below.
+
+// note it will change in the original array too.
+
+
+const a = ["apple", "ball","cat", "dog", "eagle" ,"fan"];
+
+// console.log(a.copyWithin(2)) ;
+
+// here it copies first value to the the index[2] i.e apple is stored and it will continue from it.
+
+
+// by using two parameters
+
+// console.log(a.copyWithin(1,2)); // here first parameter is to target the index number. and second parameter is used to find the index  and passed the array value to the first parameter. and then it will continue the sequence to the end . and the rest of the array will place in their same places
+
+// i.e 0 is an index and finds  index-2 of that array value(cat) is stored to 0 index. and continue till end. and rest of them in their places
+
+
+console.log(a.copyWithin(1,2,3)) // first one it targets and second parameter is used to the start and third is used to end.
+
+// in simpler above ex:-  2 index value(cat) is stored in index 1 and continue to index 3 (cat)
