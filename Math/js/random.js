3172275dd4e2d410ae8eef2ee91df2d74545af09
mohammedyaseen2211 <ysn6040@gmail.com>
2022-11-06 23:12:07 +0530

    Math methods are added to file

---
+++ Math/js/random.js
@@ -0,0 +1,11 @@
+//  math.random() function return a floating-point, pseudo- random number that greater than or equal to o and less than 1.
+
+//  it cananot be chosen or reset by the user.
+
+
+var a = 20;
+var b = 10;
+
+console.log(Math.random(a) );
+
+
