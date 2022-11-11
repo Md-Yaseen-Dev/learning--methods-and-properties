121b09b2d374ddfd1fb8d60a08bb1a35b6f0461b
mohammedyaseen2211 <ysn6040@gmail.com>
2022-11-11 23:18:13 +0530

    filter method is completed

--- array/js/filter.js
+++ array/js/filter.js
@@ -52,3 +52,83 @@ function isprime(num) {
 }
 
 console.log(array.filter(isprime));
+
+// --- Filtering invalid entries from json.
+
+// The following example uses filter() to create a filtered json of all elments with non-zero , numeric id.
+
+
+const arr = [
+    { id: 15 },
+    { id: 1 },
+    { id: 0 },
+    { id: 3 },
+    { id: 5 },
+    { id: 12 },
+    {},
+    { id: null },
+    { id: NaN },
+    { id: undefined },
+
+];
+
+let invalidEntries = 0;
+
+function filterByID(item) {
+    if (Number.isFinite(item.id) && item.id !== 0) {
+        return true;
+    }
+    invalidEntries++;
+    return false;
+}
+
+const arrByIc = arr.filter(filterByID);
+
+console.log('filtered array \n', arrByIc); // it return other than zero values such as 0, NaN, undefined, null etc.
+
+console.log('Number of invalid Entries = ', invalidEntries) // it returns the number of invalid entries.
+
+
+//  ----- searching in array
+
+// following examples uses filter() to filter array content based on search criteria.
+
+const fruits = ['apple', 'grapes', 'strawberry', 'mango', 'pineapple'];
+
+// filter array search for criteria(query)
+
+function filteritems(arr, query) {
+
+    return arr.filter((el) => 
+
+        el.toLowerCase().includes(query.toLowerCase()));
+}
+
+console.log(filteritems(fruits,'ap'))
+console.log(filteritems(fruits,'s'))
+
+
+// ----- using filter() on sparse arrays
+
+// filter will skip empty slots
+
+
+console.log([1,, undefined].filter((x)=> x ===undefined));
+console.log([1,, undefined].filter((x)=> x !== 2));
+
+
+// --- calling filter() on non array objects
+
+// the filter() methods reads the length property of this and then accesses each integer index.
+
+const arraylike = {
+    length : 3,
+
+  0 :"a",
+  1 :"b",
+  2 :"c",
+
+};
+console.log(
+    Array.prototype.filter.call(arraylike,(x)=> x <= "b")
+)
