043bb457d92d1cb914df82a7a3f60d7c9511debc
mohammedyaseen2211 <ysn6040@gmail.com>
2022-11-15 00:28:45 +0530

    completed

--- array/js/forEach.js
+++ array/js/forEach.js
@@ -27,6 +27,7 @@ function myfunc(b) {
 // here it work same as above.
 
 
+// ---- aysnc function as forEach function
 
 const ratings = [5, 4, 5];
 
@@ -63,8 +64,84 @@ const copyitems = [];
 console.log(copyitems);
 
 // before
-    for (let i = 0; i <= items.length; i++) {
-        copyitems.push(items[i])
-    } 
+for (let i = 0; i < items.length; i++) {
+    copyitems.push(items[i])
+}
 console.log(copyitems);
 // after 
+
+items.forEach((items) => {
+    copyitems.push(items);
+});
+// console.log(copyitems);
+
+
+
+// ------printing the contents of the array
+
+const logArrayelements = (element1, index /*array*/) => {
+    console.log(`a[${index}] = ${element1}`)
+};
+
+
+[2.3, 5, 8].forEach(logArrayelements);
+
+
+//  ---- using the args
+
+// the following example updates an objects properties from each entry in the array,
+
+class counter {
+
+    constructor() {
+        this.sum = 0;
+        this.count = 0;
+    }
+
+    add(array) {
+
+        array.forEach(function countEntry(entry) {
+            this.sum += entry;
+            ++this.count;
+
+        }, this);
+    }
+}
+
+const obj = new counter();
+obj.add([2, 3, 4]);
+
+console.log(obj.count);
+console.log(obj.sum); // it returns the sum of the array.
+
+
+// -- modifying the array  during iteration
+
+
+const words = ["one", "two", "three", "four"]
+
+words.forEach((word) => {
+    console.log(word);
+
+    // modifying
+
+    if (word === "two") {
+        words.shift();
+    }
+});
+console.log(words);
+
+// ----- calling forEach() on non array objects
+
+// the forEach() method reads the length property of this and then access each integer index
+
+const arraylike = {
+    length :3,
+    0: 2,
+    1: 4,
+    2: 6,
+};
+
+Array.prototype.forEach.call(arraylike, (x) => console.log(x))
+
+// console.log(arraylike);
